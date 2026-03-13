12aa9ded5bb86b1ebe2ba4df09c81e69ae93e610
u679c <3178968816@qq.com>
2026-03-13 14:39:03 +0800

    创建项目，实现了导航

---
+++ app/src/main/java/com/example/kpappercutting/ui/screens/CreateScreen.kt
@@ -0,0 +1,10 @@
+package com.example.kpappercutting.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CreateScreen() {
+    Text(text = "创作 - Create")
+}
+
---
+++ app/src/main/java/com/example/kpappercutting/ui/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.kpappercutting.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "首页 - Home")
+}
+
